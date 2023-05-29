5770fe189ebcb8bf87d81b358928aaccf43d724a
Rstam <ganizoda.rstam@gmail.com>
2023-05-29 20:31:00 +0200

    Project creation
    Project is created in Xcode IDE.

---
+++ Currency Converter/Currency Converter/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Currency Converter
+//
+//  Created by Rstam Ganizoda on 29/05/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+

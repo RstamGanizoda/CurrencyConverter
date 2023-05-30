f498183342c2333aa792d2ed0a53f271d389bad0
Rstam <ganizoda.rstam@gmail.com>
2023-05-30 14:25:11 +0200

    syncButton added
    syncButton added with animations

--- Currency Converter/Currency Converter/Controllers/mainViewController.swift
+++ Currency Converter/Currency Converter/Controllers/mainViewController.swift
@@ -1,19 +1,53 @@
-//
-//  ViewController.swift
-//  Currency Converter
-//
-//  Created by Rstam Ganizoda on 29/05/2023.
-//
-
 import UIKit
 
-class mainViewController: UIViewController {
 
+private extension Double {
+    
+    static var durationForButtonClick: Double = 0.2
+    static var scalesForButtonClick: Double = 0.9
+}
+class mainViewController: UIViewController {
+    
+    @IBOutlet weak var syncButton: UIButton!
+    
+    @IBOutlet weak var firstCurrencyFlagImageView: UIImageView!
+    @IBOutlet weak var secondCurrencyFlagImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+      
         
         
     }
+    
+    
+    @IBAction func syncButtonPressed(_ sender: UIButton) {
+        print("hello")
+        syncButton.zoomInOut()
+    }
+    
+    
 }
 
+extension UIView {
+    func zoomInOut(duration: TimeInterval = .durationForButtonClick) {
+        UIView.animate(
+            withDuration: duration,
+            delay: .zero,
+            options: .curveLinear,
+            animations: {
+            self.transform = CGAffineTransform(scaleX: CGFloat(.scalesForButtonClick),
+                                               y: CGFloat(.scalesForButtonClick))
+            },
+            completion: { _ in
+                UIView.animate(
+                    withDuration: duration,
+                    delay: .zero,
+                    options: .curveLinear
+                ){
+                    self.transform = CGAffineTransform.identity
+                }
+            })
+    }
+}

cce085ad94de1150e06f45f755bce4cb82f9fa5c
Rstam <ganizoda.rstam@gmail.com>
2023-05-30 19:12:34 +0200

    Converter View adjustments

--- Currency Converter/Currency Converter/Controllers/mainViewController.swift
+++ Currency Converter/Currency Converter/Controllers/mainViewController.swift
@@ -1,18 +1,45 @@
 import UIKit
 
+private extension UIColor {
+    static let textColor: UIColor = UIColor(
+        red: 22.0/255.0,
+        green: 63.0/255.0,
+        blue: 131.0/255.0,
+        alpha: 1.0)
+}
+private extension CGFloat {
+    static let cornerRadius: CGFloat = 20
+    static let paddingTextField: CGFloat = 10
+}
+
+private extension String {
+    static let placeholderComment = "0000.00"
+}
+
 class mainViewController: UIViewController {
     
     @IBOutlet weak var syncButton: UIButton!
-    
+    @IBOutlet weak var converterView: UIView!
     @IBOutlet weak var firstCurrencyFlagImageView: UIImageView!
     @IBOutlet weak var secondCurrencyFlagImageView: UIImageView!
+    @IBOutlet weak var firstCurrencyLabel: UILabel!
+    @IBOutlet weak var secondCurrencyLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var convertedAmountLabel: UILabel!
+    @IBOutlet weak var exchangeRateLabel: UILabel!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var convertedAmountTextField: UITextField!
+    @IBOutlet weak var mainSubtitle: UILabel!
+    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var exchangeRateTitleLable: UILabel!
+    @IBOutlet weak var secondDropDownButton: UIButton!
+    @IBOutlet weak var firstDropDownButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
-        
-        
+        modifyConverterView()
+        modifyCurrencyFlag()
+        modifyAmountTextFields()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -21,9 +48,60 @@ class mainViewController: UIViewController {
     
     
     @IBAction func syncButtonPressed(_ sender: UIButton) {
-        print("hello")
         syncButton.zoomInOut()
     }
-  
     
+    @IBAction func firstDropDownButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func secondDropDownButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    private func modifyConverterView() {
+        converterView.layer.cornerRadius = .cornerRadius
+        converterView.backgroundColor = .white
+        converterView.dropShadow()
+    }
+    
+    private func modifyCurrencyFlag() {
+        firstCurrencyFlagImageView.layer.cornerRadius = firstCurrencyFlagImageView.frame.width / 2
+        secondCurrencyFlagImageView.layer.cornerRadius = secondCurrencyFlagImageView.frame.width / 2
+    }
+    
+    private func modifyAmountTextFields() {
+        amountTextField.textFieldParameters()
+        
+        let placeholderComment = NSAttributedString(
+            string: .placeholderComment,
+            attributes: [NSAttributedString
+                .Key
+                .foregroundColor: UIColor.textColor]
+        )
+        amountTextField.attributedPlaceholder = placeholderComment
+        amountTextField.textFieldsSettings()
+        amountTextField.textFieldParameters()
+        convertedAmountTextField.textFieldsSettings()
+        convertedAmountTextField.textFieldParameters()
+    }
+}
+
+extension mainViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        amountTextField.resignFirstResponder()
+        convertedAmountLabel.resignFirstResponder()
+        return true
+    }
+}
+
+extension UITextField {
+    func textFieldsSettings() {
+        self.textColor = .textColor
+        self.rightViewMode = .always
+        self.clearButtonMode = .always
+        self.clearsOnBeginEditing = true
+        self.autocapitalizationType = .words
+        self.borderStyle = .none
+        self.font = UIFont(name: "Helvetica", size: 22)
+    }
 }
--- Currency Converter/Currency Converter/Extensions/Extension + UIView.swift
+++ Currency Converter/Currency Converter/Extensions/Extension + UIView.swift
@@ -1,16 +1,44 @@
 import UIKit
 
 private extension Double {
-    static var durationForButtonClick: Double = 0.2
-    static var scalesForButtonClick: Double = 0.9
+    static let durationForButtonClick: Double = 0.2
+    static let scalesForButtonClick: Double = 0.9
+    static let widthShadowOffset: Double = 5.0
+    static let heightShadowOffset: Double = 8.0
+    static let shadowOpacity: Double = 0.5
+}
+
+private extension CGFloat {
+    static let shadowRadius: CGFloat = 20
+    static let textFieldCornerRadius: CGFloat = 10
+    static let alphaForTextField: CGFloat = 0.2
 }
 
 private extension UIColor {
-    static var topColor: UIColor = UIColor(red: 216.0/255.0, green: 197.0/255.0, blue: 200.0/255.0, alpha: 1.0)
-    static var bottomColor: UIColor = UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)
+    static let topColor: UIColor = UIColor(
+        red: 216.0/255.0,
+        green: 197.0/255.0,
+        blue: 200.0/255.0,
+        alpha: 1.0
+    )
+    static let bottomColor: UIColor = UIColor(
+        red: 247.0/255.0,
+        green: 247.0/255.0,
+        blue: 247.0/255.0,
+        alpha: 1.0
+    )
 }
 
 extension UIView {
+    func textFieldParameters(
+          cornerRadius: CGFloat = .textFieldCornerRadius,
+          backgroundColor: UIColor = .systemGray3.withAlphaComponent(.alphaForTextField)
+      ){
+          self.layer.cornerRadius = cornerRadius
+          self.backgroundColor = backgroundColor
+          self.layer.masksToBounds = true
+      }
+    
     func zoomInOut(duration: TimeInterval = .durationForButtonClick) {
         UIView.animate(
             withDuration: duration,
@@ -40,4 +68,19 @@ extension UIView {
         gradientLayer.frame = self.bounds
         self.layer.insertSublayer(gradientLayer, at:0)
     }
+    
+    func dropShadow() {
+            self.layer.masksToBounds = false
+            self.layer.shadowColor = UIColor.black.cgColor
+            self.layer.shadowOpacity = Float(.shadowOpacity)
+            self.layer.shadowOffset = CGSize(
+                width: .widthShadowOffset,
+                height: .heightShadowOffset
+            )
+            self.layer.shadowRadius = .shadowRadius
+            self.layer.shadowPath = UIBezierPath(
+                roundedRect: self.bounds,
+                cornerRadius: self.layer.cornerRadius
+            ).cgPath
+        }
 }
